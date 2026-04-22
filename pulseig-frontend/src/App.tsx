3672b97696868d3f4c43abb38c96dafc79f7db83
Roadmap10k <jema.comercio@gmail.com>
2026-04-22 00:18:24 -0300

    Add Reengagement and Settings pages to routes

--- pulseig-frontend/src/App.tsx
+++ pulseig-frontend/src/App.tsx
@@ -4,10 +4,10 @@ import { useAuthStore } from './store';
 import Layout from './components/layout/Layout';
 import LoginPage from './pages/LoginPage';
 import InboxPage from './pages/InboxPage';
-import ContactsPage from './pages/ContactsPage';
-import ProductsPage from './pages/ProductsPage';
-import TriggersPage from './pages/TriggersPage';
 import DashboardPage from './pages/DashboardPage';
+import ReengagementPage from './pages/ReengagementPage';
+import SettingsPage from './pages/SettingsPage';
+import { ContactsPage, ProductsPage, TriggersPage } from './pages/ResourcePages';
 import './index.css';
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
@@ -29,6 +29,8 @@ export default function App() {
           <Route path="products" element={<ProductsPage />} />
           <Route path="triggers" element={<TriggersPage />} />
           <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="reengagement" element={<ReengagementPage />} />
+          <Route path="settings" element={<SettingsPage />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
